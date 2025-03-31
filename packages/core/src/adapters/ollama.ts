e9d05cb409f9dfd712401ea00110bf4cd1f8a49b
MiaowFISH <neko-project@outlook.com>
2025-04-01 00:04:48 +0800

    refactor(adapters): 重构适配器模块
    - 删除消息组件定义
    - 使用 xsai 实现适配器功能
    - 使用 zod 作为类型检查器
    - 支持原生工具调用
    - 支持流式输出
    - 更好的工具参数类型提示

--- packages/core/src/adapters/ollama.ts
+++ packages/core/src/adapters/ollama.ts
@@ -1,103 +1,83 @@
+import { createOllama } from "@xsai-ext/providers-local";
+import { generateText, GenerateTextResult } from '@xsai/generate-text';
+import { ChatOptions, Message } from '@xsai/shared-chat';
+import { streamText, StreamTextResult } from '@xsai/stream-text';
+import { ToolResult } from '@xsai/tool';
+
 import { Config } from "../config";
-import { sendRequest } from "../utils/http";
-import { BaseAdapter, Response } from "./base";
+import { BaseAdapter } from "./base";
 import { LLM } from "./config";
-import { Message } from "./creators/component";
-import { ToolSchema } from "./creators/schema";
 
-interface ToolCall {
-  function: {
-    name: string;
-    arguments: {
-      [key: string]: any;
-    }
-  }
-}
-
-interface ToolMessage {
-  role: "tool";
-  content: string;
-}
-
-function ToolMessage(content: string): ToolMessage {
-  return {
-    role: "tool",
-    content
-  }
-}
 
 export class OllamaAdapter extends BaseAdapter {
-  private config: LLM;
-  constructor(config: LLM, parameters?: Config["Parameters"]) {
-    super(config, parameters);
-    this.url = `${config.BaseURL}/api/chat`;
-    this.config = config;
-  }
-
-  async chat(messages: Message[], toolsSchema?: ToolSchema[], debug = false): Promise<Response> {
-    for (const message of messages) {
-      let content = "";
-      for (const component of message.content) {
-        if (typeof component === "string"){
-          content += component;
-        } else if (component.type === "image_url") {
-          if (!message["images"]) message["images"] = [];
-          message["images"].push(component["image_url"]["url"]);
-        } else if (component.type === "text") {
-          content += component["text"];
+    private provider: any;
+    constructor(private config: LLM, parameters?: Config["Parameters"]) {
+        super(config, parameters);
+        if (!this.baseURL) {
+            throw new Error('BaseURL is required for OllamaAdapter');
         }
-      }
-      message["content"] = content;
-    }
-    const requestBody = {
-      model: this.model,
-      format: this.ability.includes("结构化输出") ? "json" : undefined,
-      // reasoning_effort: this.ability.includes("深度思考") ? this.reasoningEffort : undefined,
-      messages,
-      tools: toolsSchema,
-      options: {
-        numa: this.config.NUMA,
-        num_ctx: this.config.NumCtx,
-        num_batch: this.config.NumBatch,
-        num_gpu: this.config.NumGPU,
-        main_gpu: this.config.MainGPU,
-        low_vram: this.config.LowVRAM,
-        logits_all: this.config.LogitsAll,
-        vocab_only: this.config.VocabOnly,
-        use_mmap: this.config.UseMMap,
-        use_mlock: this.config.UseMLock,
-        num_thread: this.config.NumThread,
-        // 以上是加载模型时要用到的参数
-        // 以下是推理时要用到的参数
-        num_predict: this.parameters?.MaxTokens,
-        temperature: this.parameters?.Temperature,
-        top_p: this.parameters?.TopP,
-        presence_penalty: this.parameters?.PresencePenalty,
-        frequency_penalty: this.parameters?.FrequencyPenalty,
-        stop: this.parameters?.Stop,
-        ...this.otherParams,
-      },
-    };
-    let response = await sendRequest(this.url, this.apiKey, requestBody, this.adapterConfig.Timeout, debug);
 
-    try {
-      return {
-        model: response.model,
-        created: response.created_at,
-        message: {
-          role: response.message.role,
-          content: response.message.content,
-          tool_calls: response.message.tool_calls,
-        },
-        usage: {
-          prompt_tokens: response.prompt_eval_count,
-          completion_tokens: response.eval_count,
-          total_tokens: response.prompt_eval_count + response.eval_count,
-        },
-      };
-    } catch (error) {
-      console.error("Error parsing response:", error);
-      console.error("Response:", response);
+        this.provider = createOllama(
+            config.BaseURL
+        );
     }
-  }
+
+    async chat(messages: Message[], toolsSchema?: ToolResult[], debug = false): Promise<GenerateTextResult> {
+        // 公共参数
+        const chatOptions: ChatOptions = {
+            ...this.provider.chat(this.model),
+            frequencyPenalty: this.parameters.FrequencyPenalty,
+            messages,
+            presencePenalty: this.parameters.PresencePenalty,
+            // seed
+            //@ts-ignore
+            stop: this.parameters.Stop,
+            temperature: this.parameters.Temperature,
+            // toolChoice
+            topP: this.parameters.TopP,
+        }
+
+        if (this.ability.includes("流式输出")) {
+            const result = await streamText({
+                ...chatOptions,
+                ...(toolsSchema ? { tools: toolsSchema } : {}),
+                // maxSteps
+                ...this.otherParams,
+                streamOptions: {
+                    usage: true,
+                }
+            })
+            let fullContent = "";
+            let currentLineBuffer = "";
+            for await (const textPart of result["textStream"]) {
+                fullContent += textPart;
+                currentLineBuffer += textPart;
+                if (debug) {
+                    if (currentLineBuffer.includes("\n")) {
+                        // 清除当前行并将光标移动到行首
+                        process.stdout.write('\x1B[K\r');
+                        // 输出新的文本
+                        process.stdout.write(currentLineBuffer);
+                        // 重置当前行缓冲区
+                        currentLineBuffer = "";
+                    }
+                }
+            }
+            for await (const step of result["stepStream"]) {
+                return {
+                    ...step,
+                    text: fullContent,
+                } as unknown as GenerateTextResult;
+            }
+        }
+
+        // 非流式输出
+        const result = await generateText({
+            ...chatOptions,
+            ...(toolsSchema ? { tools: toolsSchema } : {}),
+            // reasoning_effort: this.ability.includes("深度思考")? this.reasoningEffort : undefined, 这部分可以通过 OtherParameters 实现
+            ...this.otherParams,
+        });
+        return result;
+    }
 }
