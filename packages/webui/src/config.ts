83b48ee6f71816a28424ece87609b90e600cecb1
MiaowFISH <neko-project@outlook.com>
2025-04-12 11:37:06 +0800

    chore: 暂时移除webui和extension相关代码

--- packages/webui/src/config.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from "koishi";
-
-export interface Config {
-  Debug: boolean;
-}
-
-export const Config: Schema<Config> = Schema.object({
-  Debug: Schema.boolean().default(false).description("调试模式")
-});
