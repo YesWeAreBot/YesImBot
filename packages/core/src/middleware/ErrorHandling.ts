ac2d5b1d32b85183578fb2535a202c61029a7edf
MiaowFISH <neko-project@outlook.com>
2025-05-16 18:25:54 +0800

    fix: 更改错误上报地址

--- packages/core/src/middleware/ErrorHandling.ts
+++ packages/core/src/middleware/ErrorHandling.ts
@@ -67,7 +67,7 @@ export class ErrorHandlingMiddleware implements Middleware {
             const formData = new FormData();
             formData.append('c', content);
 
-            const response = await fetch('https://dump.thfls.club/', {
+            const response = await fetch('https://dump.yesimbot.chat/', {
                 method: 'POST',
                 body: formData,
                 headers: {
