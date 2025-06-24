addf8646488f3b198b565908c08f3932629bebde
MiaowFISH <neko-project@outlook.com>
2025-06-24 17:04:38 +0800

    refactor(core): 移除中间件模块及相关代码

--- packages/core/src/middleware/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./base";
-export * from "./condition.middleware";
-export * from "./error.middleware";
-export * from "./reasoning/middleware";
--- packages/core/src/shared/index.ts
+++ packages/core/src/shared/index.ts
@@ -1,6 +1,4 @@
 export * from "./config";
-export * from "./constants";
 export * from "./database";
 export * from "./errors";
-export * from "./types";
 export * from "./utils";
