215bf0ff2d6077bafe8eaba9c8d77ae9c419a409
MiaowFISH <neko-project@outlook.com>
2025-08-04 23:12:53 +0800

    chore(root): 🔨 添加作用域

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -36,7 +36,7 @@ export default {
         useEmoji: true,
         emojiAlign: 'center',
         themeColorCode: '',
-        scopes: ['root', 'core', 'mcp', 'llm', 'memory', 'config'],
+        scopes: ['root', 'core', 'mcp', 'interpreter', 'daily', 'favor', 'sticker'],
         allowCustomScopes: true,
         allowEmptyScopes: true,
         customScopesAlign: 'bottom',
