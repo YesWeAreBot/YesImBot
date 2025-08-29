b02e7bedb236de5577fc64f2ffa731ce7629f548
MiaowFISH <neko-project@outlook.com>
2025-08-29 21:39:37 +0800

    refactor: 移除配置向导服务并简化内存服务初始化

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,5 @@
 import { Context, ForkScope, Service, sleep } from "koishi";
 import { AgentCore } from "./agent";
-import { ConfiguratorService } from "./commands/configurator";
 import { Config } from "./config";
 import { AssetService, LoggerService, MemoryService, ModelService, PromptService, ToolService, WorldStateService } from "./services";
 import { handleError, initializeErrorReporter } from "./shared/errors";
@@ -19,8 +18,8 @@ export default class YesImBot extends Service<Config> {
     };
     static readonly name = "yesimbot";
     static readonly usage = `"Yes! I'm Bot!" 是一个能让你的机器人激活灵魂的插件。\n
-使用请阅读 [文档](https://docs.yesimbot.chat/) ，推荐使用 [GPTGOD](https://gptgod.online/#/register?invite_code=envrd6lsla9nydtipzrbvid2r) 提供的 \`deepseek-v3\` 模型以获得最高性价比。目前已知效果最佳模型：\`gemini-2.5-pro-preview-06-05
-\`\n
+使用请阅读 [文档](https://docs.yesimbot.chat/) ，推荐使用 [GPTGOD](https://gptgod.online/#/register?invite_code=envrd6lsla9nydtipzrbvid2r) 提供的 \`deepseek-v3\` 模型以获得最高性价比。目前已知效果最佳模型：\`gemini-2.5-pro-preview-06-05\`
+\n
 官方交流群：[857518324](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=k3O5_1kNFJMERGxBOj1ci43jHvLvfru9&authKey=TkOxmhIa6kEQxULtJ0oMVU9FxoY2XNiA%2B7bQ4K%2FNx5%2F8C8ToakYZeDnQjL%2B31Rx%2B&noverify=0&group_code=857518324)\n`;
     constructor(ctx: Context, config: Config) {
         super(ctx, "yesimbot", true);
@@ -42,7 +41,7 @@ export default class YesImBot extends Service<Config> {
             const modelService = ctx.plugin(ModelService, { ...config.modelService, system: config.system });
 
             // 注册记忆管理层
-            const memoryService = ctx.plugin(MemoryService, { ...config.capabilities.memory, system: config.system });
+            const memoryService = ctx.plugin(MemoryService, config.capabilities.memory);
 
             // 注册 WorldState 服务
             const worldStateService = ctx.plugin(WorldStateService, {
@@ -80,8 +79,6 @@ export default class YesImBot extends Service<Config> {
             handleError(this.ctx.logger("[YesImBot]"), error, "初始化时发生错误");
             this.ctx.stop();
         }
-
-        ctx.plugin(ConfiguratorService, config);
     }
 }
 
